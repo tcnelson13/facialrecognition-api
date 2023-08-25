6a2f016a69bd89d9197fa80f5384031dbc57092c
Thomas Nelson <tcnelson13@gmail.com>
2023-08-25 09:03:07 -0500

    prod db changes

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const cors = require("cors");
+// const db = require("knex")({
+//   client: "pg",
+//   connection: {
+//     host: "127.0.0.1",
+//     port: 5432,
+//     user: "",
+//     password: "",
+//     database: "Facial_Recognition",
+//   },
+// });
+
 const db = require("knex")({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    connectionString: process.env.DATABASE_URL,
+    host: process.env.DATABASE_HOST,
     port: 5432,
-    user: "",
-    password: "",
-    database: "Facial_Recognition",
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PW,
+    database: process.env.DATABASE_DB,
   },
 });
 
